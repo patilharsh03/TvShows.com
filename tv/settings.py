a65c885306555f997221b8d966b3cde80442e315
Harsh Patil <harshpatil223@gmail.com>
2020-10-06 16:42:49 +0530

    initial commit

--- tv/settings.py
+++ tv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ib^nav^#k^sow!(xw@$61w-q=%+8j%3b^ju*yc+z=hy#fq1i)b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
